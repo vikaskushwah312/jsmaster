77892d6f6eab52ac6873415b2de6e116a72d82f0
ucodeworld <vikaskushwah312@gmail.com>
2022-11-25 09:03:31 +0530

    todo done

--- react/project/todo/src/App.js
+++ react/project/todo/src/App.js
@@ -1,23 +1,79 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Pickup dry cleaning',
+      isCompleted: true,
+    },
+    {
+      content: 'Get haircut',
+      isCompleted: false,
+    },
+    {
+      content: 'Build a todo app in React',
+      isCompleted: false,
+    }
+  ]);
+  function handleKeyDown(e,i){
+    if(e.key === 'Enter'){
+      createTodoAtIndex(e,i);
+    }
+    if(e.key === 'Backspace' && todos[i].content ===''){
+      e.preventDefault();
+      removeTodoAtIndex(i);
+    }
+  }
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function createTodoAtIndex(e,i){
+    const newTodos = [...todos];
+    newTodos.splice(i+1,0,{content:'',isCompleted:false});
+    setTodos(newTodos);
+    setTimeout(()=>{
+      document.forms[0].elements[i+1].focus();
+    },5)
+  }
+
+  function updateTodoAtIndex(e,i){
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+  function toggleTodoCompleteAtIndex(index) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
+    setTodos(temporaryTodos);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className="todo-list">
+        <ul>
+          { todos.map((todo,i)=>{
+            // console.log(todo,i,'ram');
+            return (
+              <div className="todo" key={i}>
+                <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
+                  {todo.isCompleted && (<span>&#x2714;</span>)}
+                </div>
+                  <input type="text" value={todo.content}  onKeyDown={e=>handleKeyDown(e,i)}
+                  onChange={(e)=>updateTodoAtIndex(e,i)} />
+              </div>
+            )
+          })}
+        </ul>
+      </form>
     </div>
   );
 }
--- react/project/todo/src/index.js
+++ react/project/todo/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDom.render(<App />,document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
